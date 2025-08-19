f11128a85f7266f224de0ce8a59d2fe0e55643b4
bgao <1223129433@qq.com>
2025-08-19 23:37:18 +0800

    test commit

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const app = require('express')();
-const wsInstance = require('express-ws')(app);
+const app = require("express")();
+const fs = require("fs");
+const https = require("https");
 
-app.ws('/', ws => {
-	ws.on('message', data => {
-		// 未做业务处理，收到消息后直接广播
-		wsInstance.getWss().clients.forEach(server => {
-			if (server !== ws) {
-				server.send(data);
-			}
-		});
-	});
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
+
+const server = https.createServer(options, app); // 创建 HTTPS 服务器实例
+const wsInstance = require("express-ws")(app, server); // 正确初始化 wsInstance
+
+app.ws("/", (ws) => {
+  ws.on("message", (data) => {
+    // 未做业务处理，收到消息后直接广播
+    wsInstance.getWss().clients.forEach((client) => {
+      if (client !== ws) {
+        client.send(data);
+      }
+    });
+  });
 });
 
-app.get('/', (req, res) => {
-	res.sendFile('./client/index.html', { root: __dirname });
+app.get("/", (req, res) => {
+  res.sendFile("./client/index.html", { root: __dirname });
 });
 
-app.get('/p2p', (req, res) => {
-	res.sendFile('./client/p2p.html', { root: __dirname });
+app.get("/p2p", (req, res) => {
+  res.sendFile("./client/p2p.html", { root: __dirname });
 });
 
-app.listen(8080);
+server.listen(8443, "0.0.0.0", () => {
+  console.log("HTTPS server listening on port 8443");
+});
